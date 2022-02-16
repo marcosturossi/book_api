c86cbfbadc9b2f2c2dd4d7e8419018e46aa6ec50
Marcos Turossi <marcosturossi@gmail.com>
2022-02-16 14:22:53 -0300

    Criando os models

--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+    autor = models.CharField(max_length=255)
+    page_number = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Collection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+
